46ca6c65887ce3b147f261a10f8890b8dedf44ce
SugitaMotoki <ee197011@meiji.ac.jp>
2022-06-16 01:55:22 +0900

    コンポーネントの練習

---
+++ components/Counter.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <p>{{ $count }}</p>
+    <v-btn @click="countUp">Count UP</v-btn>
+  </div>
+</template>
+
+<script setup lang="ts">
+const $count = ref(0);
+const countUp = () => {
+  $count.value++;
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-container>
+    <h1>Nuxt!!</h1>
+    <Counter/>
+    <Counter/>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+</script>
