d55b77dbf2cb763e31f0ed5e2b1dc76676cf9c85
SugitaMotoki <ee197011@meiji.ac.jp>
2022-06-16 00:49:22 +0900

    initial

---
+++ app.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="event-handling">
+    <p>{{ count }}</p>
+    <button @click="increment">Increment</button>
+    <button @click="ab">AB</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+const count = ref(0);
+const increment = () => {
+  count.value++;
+}
+function ab() {
+  console.log("aa");
+}
+</script>
+
+<style scoped>
+</style>
