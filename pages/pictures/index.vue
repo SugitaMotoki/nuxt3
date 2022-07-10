266a7446cc49ae1b5a0c1c2ec5d141de84c96a7e
SugitaMotoki <ee197011@meiji.ac.jp>
2022-07-10 16:27:50 +0900

    ページ遷移の練習

---
+++ pages/pictures/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-container>
+    <h1>写真</h1>
+    <NuxtLink to="/pictures/new">新規追加</NuxtLink>
+    <Counter/>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+</script>
---
+++ pages/pictures/new.vue
@@ -0,0 +1,8 @@
+<template>
+  <v-container>
+    <h1>写真の新規追加</h1>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+</script>
